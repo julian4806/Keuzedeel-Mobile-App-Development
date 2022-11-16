7bdbe3d71e457d68e77e4f48e115df1875ebc233
Julian4806 <210549@student.glu.nl>
2022-11-16 16:53:18 +0100

    added a platformer!

---
+++ Eindopdracht/src/scenes/Game.js
@@ -0,0 +1,40 @@
+import Phaser from "../lib/phaser.js";
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+
+  preload() {
+    this.load.image("background", "assets/bg_layer1.png");
+    this.load.image("platform", "assets/ground_grass.png");
+
+    this.load.image("bunny-stand", "assets/bunny1_stand.png");
+  }
+
+  create() {
+    this.add.image(240, 320, "background");
+
+    const platforms = this.physics.add.staticGroup();
+
+    for (let i = 0; i < 5; ++i) {
+      const x = Phaser.Math.Between(80, 400);
+      const y = 150 * i;
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = platforms.create(x, y, "platform");
+      platform.scale = 0.5;
+
+      /** @type {Phaser.Physics.Arcade.StaticBody} */
+      const body = platform.body;
+      body.updateFromGameObject();
+    }
+    const player = this.physics.add
+      .sprite(240, 320, "bunny-stand")
+      .setScale(0.5);
+
+    this.physics.add.collider(platforms, player);
+  }
+}
+// page 23
+// http://192.168.178.88/infinite-jumper/Infinite_Jumper_Phaser3_Modern_JavaScript.pdf
