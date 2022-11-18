2ef12315e72543f4dc7bb44a1ae620b00abc70cb
Julian van Husen <210549@student.glu.nl>
2022-11-18 15:06:33 +0100

    gone further into the tutorial

--- Eindopdracht/src/scenes/Game.js
+++ Eindopdracht/src/scenes/Game.js
@@ -1,6 +1,13 @@
 import Phaser from "../lib/phaser.js";
 
 export default class Game extends Phaser.Scene {
+  /** @type {Phaser.Physics.Arcade.StaticGroup} */
+  platforms;
+  /** @type {Phaser.Physics.Arcade.Sprite} */
+  player;
+  /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+  cursors;
+
   constructor() {
     super("game");
   }
@@ -8,32 +15,68 @@ export default class Game extends Phaser.Scene {
   preload() {
     this.load.image("background", "assets/bg_layer1.png");
     this.load.image("platform", "assets/ground_grass.png");
-
     this.load.image("bunny-stand", "assets/bunny1_stand.png");
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   create() {
-    this.add.image(240, 320, "background");
+    this.add.image(240, 320, "background").setScrollFactor(1, 0);
 
-    const platforms = this.physics.add.staticGroup();
+    this.platforms = this.physics.add.staticGroup();
 
     for (let i = 0; i < 5; ++i) {
       const x = Phaser.Math.Between(80, 400);
       const y = 150 * i;
 
       /** @type {Phaser.Physics.Arcade.Sprite} */
-      const platform = platforms.create(x, y, "platform");
+      const platform = this.platforms.create(x, y, "platform");
       platform.scale = 0.5;
 
       /** @type {Phaser.Physics.Arcade.StaticBody} */
       const body = platform.body;
       body.updateFromGameObject();
     }
-    const player = this.physics.add
+    this.player = this.physics.add
       .sprite(240, 320, "bunny-stand")
       .setScale(0.5);
 
-    this.physics.add.collider(platforms, player);
+    this.physics.add.collider(this.platforms, this.player);
+
+    this.player.body.checkCollision.up = false;
+    this.player.body.checkCollision.left = false;
+    this.player.body.checkCollision.right = false;
+    this.cameras.main.startFollow(this.player);
+
+    // set the horizontal dead zone to 1.5x game width
+    this.cameras.main.setDeadzone(this.scale.width * 1.5);
+  }
+
+  update() {
+    this.platforms.children.iterate((child) => {
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = child;
+      const scrollY = this.cameras.main.scrollY;
+      if (platform.y >= scrollY + 700) {
+        platform.y = scrollY - Phaser.Math.Between(50, 100);
+        platform.body.updateFromGameObject();
+      }
+    });
+
+    const touchingDown = this.player.body.touching.down;
+
+    if (touchingDown) {
+      this.player.setVelocityY(-300);
+    }
+
+    // left and right input logic
+    if (this.cursors.left.isDown && !touchingDown) {
+      this.player.setVelocityX(-200);
+    } else if (this.cursors.right.isDown && !touchingDown) {
+      this.player.setVelocityX(200);
+    } else {
+      // stop movement if not left or right
+      this.player.setVelocityX(0);
+    }
   }
 }
 // page 23
